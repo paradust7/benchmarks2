c77b3616b903416258fd0cb8e0dc7706e7ae11bf
paradust7 <102263465+paradust7@users.noreply.github.com>
2022-05-05 21:20:00 +0000

    fix push

--- push-revs.sh
+++ push-revs.sh
@@ -2,12 +2,11 @@
 
 MINETEST_REV=`cat minetest.rev`
 IRRLICHT_REV=`cat irrlicht.rev`
-REPO_NAME=$(basename "$GITHUB_REPOSITORY")
 
 if ! git diff-index --quiet --cached HEAD ; then
   echo "Update detected. Pushing changes"
   git commit -m "Minetest: ${MINETEST_REV} | Irrlicht: ${IRRLICHT_REV}"
-  git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${REPO_NAME}.git"
+  git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 else
   echo "No update detected"
 fi
