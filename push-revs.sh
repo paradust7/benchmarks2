3ff823d30f89d11c07bb3b3e4c090dcec944a74a
paradust7 <102263465+paradust7@users.noreply.github.com>
2022-05-05 20:07:43 +0000

    Try to fix track_minetest

--- push-revs.sh
+++ push-revs.sh
@@ -2,11 +2,12 @@
 
 MINETEST_REV=`cat minetest.rev`
 IRRLICHT_REV=`cat irrlicht.rev`
+REPO_NAME=$(basename "$GITHUB_REPOSITORY")
 
 if ! git diff-index --quiet --cached HEAD ; then
   echo "Update detected. Pushing changes"
   git commit -m "Minetest: ${MINETEST_REV} | Irrlicht: ${IRRLICHT_REV}"
-  git push
+  git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${REPO_NAME}.git"
 else
   echo "No update detected"
 fi
